7279a071c505963d2e705b6913a8f4d25fa5fde4
bharathkuppala <bharath.kuppala@simplifyvms.com>
2024-12-01 13:26:39 +0530

    terrform initial commit

--- create-ec2-instance/main.tf
+++ create-ec2-instance/main.tf
@@ -1,8 +1,3 @@
 provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
-
-resource "aws_instance" "example" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.micro"
-}
