1e6bb0a28daa1a280e69bf2022dcbf23aafc0394
bharathkuppala <bharath.kuppala@simplifyvms.com>
2025-03-31 16:38:42 +0530

    Added ami

--- create-ec2-instance/main.tf
+++ create-ec2-instance/main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "practice-instance" {
   ami           = "ami-09c813fb71547fc4f"  # Specify an appropriate AMI ID
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Practice EC2 Instance"
+  }
 }
